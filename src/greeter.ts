014ba067f773a5637e3e1ebb156dfdb1fcdcdab3
Michael Zabka <zabka.michael@gmail.com>
2015-05-15 17:58:39 +0200

    Add greet test of performance

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,13 +1,19 @@
 export class Greeter {
 	
-	private name: { cs: string; pl: string; sk: string; }
+	private name: { cs?: string; pl?: string; sk?: string; } = {};
 	get Name() { return this.name; }
+	private title: string = 'Greeting';
+	get Title() { return this.title; }
+
+	public rows: number[] = [];
 
 	constructor() {
-		this.name = {};
+		for (var i = 0;i < 10000;i++) {
+			this.rows.push(i);
+		}
 	}
 	
 	save() {
-		alert(this.name);
+		alert(JSON.stringify(this.name));
 	}
 }
